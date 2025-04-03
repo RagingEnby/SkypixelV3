4cfb9974ebefb83cdf78945d9ad8b3997dbe781e
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-04-03 19:02:31 +0000

    cleaned up disnake logging

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import constants
 logger = logging.getLogger('disnake')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='disnake.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 
