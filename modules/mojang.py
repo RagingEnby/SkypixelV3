dd38910b80463ed5493c553e99d62ca61546cf9f
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-04-26 21:53:58 +0000

    added mojang caching

--- modules/mojang.py
+++ modules/mojang.py
@@ -1,6 +1,6 @@
 import logging
-from datetime import datetime
-from typing import Any, Optional
+from datetime import datetime, timedelta
+from typing import Any
 
 from typing_extensions import TypedDict
 
@@ -8,6 +8,40 @@ import constants
 from modules import asyncreqs
 
 logger = logging.getLogger(__name__)
+
+
+class Cache:
+    TTL = timedelta(minutes=30)
+    CACHE: dict[str, "MojangPlayer"] = {}
+
+    @staticmethod
+    def cleanup():
+        now = datetime.now()
+        expired = [
+            key
+            for key, player in Cache.CACHE.items()
+            if now - player.last_updated >= Cache.TTL
+        ]
+        for key in expired:
+            del Cache.CACHE[key]
+
+    @staticmethod
+    def cache_player(player: "MojangPlayer"):
+        Cache.CACHE[player.id] = player
+        Cache.CACHE[player.name.lower()] = player
+
+    @staticmethod
+    def get(key: str) -> "MojangPlayer" | None = None:  # type: ignore[assignment]
+        Cache.cleanup()
+        player = Cache.CACHE.get(key)
+        if not player:
+            return None
+        if datetime.now() - player.last_updated < Cache.TTL:
+            logger.debug(f"cache hit for {key}: {player}")
+            return player
+        logger.debug(f"cache expired for {key}")
+        del Cache.CACHE[key]
+        return None
 
 
 class PlayerNotFound(KeyError):
@@ -26,11 +60,12 @@ class MojangPlayerDict(RawMojangPlayerDict):
 
 
 class MojangPlayer:
-    def __init__(self, name: str, uuid: str, last_updated: Optional[datetime] = None):
+    def __init__(self, name: str, uuid: str, last_updated: datetime | None = None):
         logger.debug(f"initializing MojangPlayer({name}, {uuid}, {last_updated})")
         self.name: str = name
         self.id: str = uuid
         self.last_updated: datetime = last_updated or datetime.now()
+        Cache.cache_player(self)
 
     @property
     def uuid(self) -> str:
@@ -65,6 +100,9 @@ class MojangPlayer:
 
 async def get(identifier: str) -> MojangPlayer:
     logger.debug(f"getting player {identifier}...")
+    cached = Cache.get(identifier)
+    if cached:
+        return cached
     response = await asyncreqs.get("https://api.ragingenby.dev/player/" + identifier)
     if response.status == 404:
         logger.error("invalid identifier:", identifier)
@@ -79,14 +117,22 @@ async def get(identifier: str) -> MojangPlayer:
 
 async def bulk(identifiers: list[str]) -> dict[str, MojangPlayer]:
     logger.debug(f"getting players {identifiers}...")
-    response = await asyncreqs.post(
-        url="https://api.ragingenby.dev/players",
-        json={"identifiers": [i.replace('-', '') for i in identifiers]}
-    )
-    data = await response.json()
-    logger.debug(f"got data: {data}")
-    players = [MojangPlayer.from_dict(player) for player in data['players']]
-    logger.debug(f"got players: {players}")
+    to_fetch = identifiers.copy()
+    players: list[MojangPlayer] = {}
+    for identifier in identifiers:
+        cached = Cache.get(identifier)
+        if cached:
+            players.append(cached)
+            to_fetch.remove(identifier)
+    if to_fetch:
+        response = await asyncreqs.post(
+            url="https://api.ragingenby.dev/players",
+            json={"identifiers": [i.replace('-', '') for i in identifiers]}
+        )
+        data = await response.json()
+        logger.debug(f"got data: {data}")
+        for player in data['players']:
+            players.append(MojangPlayer.from_dict(player))
     return {
         (player.id if player.id in identifiers else player.name.lower()): player
         for player in players
