adc29821469e710d79e676a1a5bd4ccc5d156b1d
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-08-09 20:50:43 +0000

    fixed on_auction_end KeyError

--- cogs/auctionhouse.py
+++ cogs/auctionhouse.py
@@ -313,7 +313,7 @@ class AuctionTrackerCog(commands.Cog):
         try:
             item = parser.decode_single(auction['item_bytes'])
         except UnicodeDecodeError as e:
-            logger.error(f"UNICODE DECODE ERROR: {e} {auction['uuid']} --- {auction['item_bytes']}")
+            logger.error(f"UNICODE DECODE ERROR: {e} {auction['auction_id']} --- {auction['item_bytes']}")
             return
         self.log_auction(auction, item, ended=True)
 
