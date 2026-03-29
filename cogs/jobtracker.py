15cdb168dff8dd3d6b4c737eb1e3796b6d51b05a
RagingEnby <github@ragingenby.dev>
2026-03-28 21:50:58 -0400

    fixed job removal

--- cogs/jobtracker.py
+++ cogs/jobtracker.py
@@ -16,61 +16,70 @@ URL: str = "https://api.ragingenby.dev/forums/jobs?cache=false"
 
 
 class Job:
-    def __init__(self, title: str, listing_id: int, qualifications: list[str], responsibilities: list[str] | None, we_can_offer: list[str]):
+    def __init__(
+        self,
+        title: str,
+        listing_id: int,
+        qualifications: list[str],
+        responsibilities: list[str] | None,
+        we_can_offer: list[str],
+    ):
         self.title = title
         self.listing_id = listing_id
         self.qualifications = qualifications
         self.responsibilities = responsibilities
         self.we_can_offer = we_can_offer
-        
+
     @property
     def clean_title(self) -> str:
         return self.title.replace(" - Minecraft Project", "")
-        
+
     @property
     def url(self) -> str:
         return f"https://hypixel.net/jobs/#listing-{self.listing_id}"
-    
+
     @property
     def application_url(self) -> str:
         return f"https://hypixel.bamboohr.com/careers/{self.listing_id}"
-    
+
     def to_markdown(self) -> str:
         lines = [f"# {self.clean_title}"]
         lines.append("## Qualifications")
         lines.extend([f"- {qualification}" for qualification in self.qualifications])
         if self.responsibilities:
             lines.append("## Responsibilities")
-            lines.extend([f"- {responsibility}" for responsibility in self.responsibilities])
+            lines.extend(
+                [f"- {responsibility}" for responsibility in self.responsibilities]
+            )
         lines.append("## We Can Offer")
         lines.extend([f"- {offer}" for offer in self.we_can_offer])
         lines.append(f"-# [Info]({self.url}) | [Apply]({self.application_url})")
         return "\n".join(lines)
-        
+
     @classmethod
-    def from_dict(cls, data: dict) -> 'Job':
+    def from_dict(cls, data: dict) -> "Job":
         return cls(
-            title=data['title'],
-            listing_id=data['listingId'],
-            qualifications=data['qualifications'],
-            responsibilities=data.get('responsibilities'),
-            we_can_offer=data['weCanOffer']
+            title=data["title"],
+            listing_id=data["listingId"],
+            qualifications=data["qualifications"],
+            responsibilities=data.get("responsibilities"),
+            we_can_offer=data["weCanOffer"],
         )
-        
+
     def to_dict(self) -> dict:
         return {
-            'title': self.title,
-            'listingId': self.listing_id,
-            'qualifications': self.qualifications,
-            'responsibilities': self.responsibilities,
-            'weCanOffer': self.we_can_offer
+            "title": self.title,
+            "listingId": self.listing_id,
+            "qualifications": self.qualifications,
+            "responsibilities": self.responsibilities,
+            "weCanOffer": self.we_can_offer,
         }
 
 
 async def get_jobs() -> list[Job]:
     logger.debug("getting job openings from api.ragingenby.dev")
     response = await asyncreqs.get(URL)
-    return [Job.from_dict(job) for job in response.json()['jobs']]
+    return [Job.from_dict(job) for job in response.json()["jobs"]]
 
 
 async def send(embed: disnake.Embed):
@@ -89,15 +98,19 @@ class JobTrackerCog(commands.Cog):
 
     @staticmethod
     async def on_job_update(job: Job, added: bool):
-        embed = utils.add_footer(disnake.Embed(
-            title=f"New Job Opening!" if added else f"Job Opening Removed!",
-            description=job.to_markdown(),
-            color=constants.COLOR_CODES['a'] if added else constants.COLOR_CODES['c']
-        ))
+        embed = utils.add_footer(
+            disnake.Embed(
+                title=f"New Job Opening!" if added else f"Job Opening Removed!",
+                description=job.to_markdown(),
+                color=constants.COLOR_CODES["a"]
+                if added
+                else constants.COLOR_CODES["c"],
+            )
+        )
         await send(embed)
-    
+
     def to_dict(self) -> dict:
-        return {job.title: job.to_dict() for job in self.data.get('jobs', [])}
+        return {job.title: job.to_dict() for job in self.data.get("jobs", [])}
 
     async def main(self):
         while True:
@@ -106,21 +119,23 @@ class JobTrackerCog(commands.Cog):
                 jobs = await get_jobs()
                 jobs_dict: dict[str, Job] = {job.title: job for job in jobs}
                 for job in jobs:
-                    if not self.data.get(job.title): 
+                    if not self.data.get(job.title):
                         await self.on_job_update(job, True)
                         self.data.data[job.title] = job
                 to_remove: set[str] = set()
                 for job in self.data.data.keys():
                     if job not in jobs_dict:
-                        await self.on_job_update(Job.from_dict(self.data.data[job]), False)
-                        to_remove.add(job.title)
+                        await self.on_job_update(
+                            Job.from_dict(self.data.data[job]), False
+                        )
+                        to_remove.add(job)
                 for key in to_remove:
                     del self.data.data[key]
                 await self.data.save()
             except Exception:
                 logger.error(traceback.format_exc())
             finally:
-                await asyncio.sleep(60*30)
+                await asyncio.sleep(60 * 30)
 
     async def close(self):
         if self.task and not self.task.done():
